9e3f4758c996f306a5c8a27435367c7e98eebc57
JeongSeonmi <pm3818@naver.com>
2023-07-20 17:07:05 +0900

    23.07.20 / 백준 14940

---
+++ 3_level/14940.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<queue>
+#define MAX 1001
+using namespace std;
+queue<pair<int, int>> q;
+int map[MAX][MAX];
+int count[MAX][MAX] = {0, };
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+int n, m;   //n : 세로, m : 가로
+
+void BFS(){
+    while(!q.empty()){
+        int curY = q.front().first;
+        int curX = q.front().second;
+        q.pop();
+        for(int i = 0; i < 4; i++){
+            int nextY = curY + dx[i];
+            int nextX = curX + dy[i];
+            if(0 <= nextY && 0 <= nextX && nextY < n && nextX < m && map[nextY][nextX] == 1 && count[nextY][nextX] == 0){
+                q.push({nextY, nextX});
+                count[nextY][nextX] = count[curY][curX] + 1;
+            }
+        }
+    }
+}
+
+int main(){
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> map[i][j];
+            if(map[i][j] == 2){
+                q.push({i, j});
+            }
+        }
+    }
+    BFS();
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(map[i][j] == 1 && count[i][j] == 0){
+                cout << "-1 ";
+            }
+            else cout << count[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
