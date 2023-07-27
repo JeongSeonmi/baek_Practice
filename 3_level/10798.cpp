104f2f4b797f32b860d721f8270ede276d5817dc
JeongSeonmi <pm3818@naver.com>
2023-07-28 07:49:57 +0900

    23.07.24 /  백준 10798 문제명, 링크 추가

--- 3_level/10798.cpp
+++ 3_level/10798.cpp
@@ -1,4 +1,6 @@
-//23.07.24
+/* 세로 읽기 */
+// 23.07.24
+// https://www.acmicpc.net/problem/10798
 
 #include<iostream>
 using namespace std;
@@ -11,7 +13,7 @@ int main(){
 
     for(int i = 0; i < 15; i++){
         for(int j = 0; j < 5; j++){
-            if(str[j][i] != '\0'){
+            if(str[j][i] != '\0'){  //문자열을 입력받을 때 마지막에 \0도 저장되므로
                 cout << str[j][i];
             }
         }
